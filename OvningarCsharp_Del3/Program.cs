eec5afd9f23a6ec61d4218a8af573bf820854a86
MagnusSandbergITHS <magnus.sandberg@iths.se>
2022-09-26 13:43:12 +0200

    Add project files.

---
+++ OvningarCsharp_Del3/Program.cs
@@ -0,0 +1,5 @@
+﻿//.29
+//Skriv en klass som representerar en bil.Varje bil ska kunna ha en färg som representeras av
+//en enum (det ska finna minst 5 olika färger på bilar), samt en längd.
+//När man instansierar en bil så ska den få en random färg, och en random längd (mellan 3 och 5 meter).
+//Instansiera 1000 bilar och spara i en array. Skriv sedan ut den sammanlagda längden av alla gröna bilar.
